78bb17ed355812648301e70faf6c7182a75f3f67
Piohi <box1234567b@gmail.com>
2024-04-22 17:35:51 +0300

    Initial Commit

---
+++ ColorPickerSUI/ColorPickerSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerSUIApp.swift
+//  ColorPickerSUI
+//
+//  Created by Anton Godunov on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
